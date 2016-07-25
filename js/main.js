e4b3dcb452bbb3bcc65abea4ee286c256e482459
gokusayon <gokusayon@gmail.com>
2016-07-25 12:50:09 +0530

    My first commit on GitHub
    Added all necessary files to implement queue.

---
+++ js/main.js
@@ -0,0 +1,43 @@
+﻿var app = angular
+    .module('app', []);
+
+app.controller('Main', ['$scope', 'ngQ', '$timeout', function($scope, ngQ, $timeout) {
+    var $el = $('#hello');
+
+
+    var func = function fun1() {
+        $timeout(function() {
+            console.log("Executing fun1");
+            $el.css("color", "brown");
+        }, 2000);
+        return "Test";
+    }
+    $scope.check = new ngQ();
+
+    $scope.check.enQueue(function fun3() {
+        $timeout(function() {
+            console.log("Executing fun3");
+            $el.css("color", "red");
+        }, 500);
+
+    });
+    $scope.check.enQueue(function fun2() {
+        $timeout(function() {
+            console.log("Executing fun2");
+            $el.css("color", "green");
+        }, 1000);
+    });
+
+    $scope.check.enQueue(func);
+
+    $scope.check.enQueue(function fun2() {
+        $timeout(function() {
+            console.log("Executing fun2");
+            $el.css("color", "green");
+        }, 1000);
+    });
+
+    $scope.check.initQueue();
+
+    $scope.hello = "This will change color";
+}]);
---
+++ js/ng-queue.js
@@ -0,0 +1,140 @@
+﻿app.factory('ngQ', ['$timeout', '$q', function($timeout, $q) {
+
+    var queue = [];
+    var name = [];
+    var result = [];
+
+    function Manager() {
+        this.queue = queue;
+        this.result = result;
+    }
+
+    function waitForAngular(callback) {
+        try {
+            var app = angular.element(document.querySelector('body'));
+            var $browser = app.injector().get('$browser');
+            $browser.notifyWhenNoOutstandingRequests(callback);
+        } catch (err) {
+            callback(err.message);
+        }
+    }
+
+    Manager.prototype = (function() {
+
+        var wrapFunction = function(fn, context, param, after) {
+            return function() {
+                name.push(fn.name);
+                var result = fn.apply(context, param);
+                after.call(this, result);
+                return result;
+            }
+        }
+
+        function _addFunction(element, params) {
+            var myFunction = wrapFunction(element, this, params, wrapFunction);
+
+            //queue.push($q.when(myFunction));
+            if (element.name)
+                Object.defineProperty(myFunction, "name", {
+                    value: element.name
+                });
+
+            queue.push(myFunction);
+            currentElement = myFunction;
+        }
+
+        function enQueue(element, params) {
+            try {
+
+                if (_isFunction(element)) {
+                    if (params == undefined || params.length == 0)
+                        params = [];
+
+                    _addFunction(element, params);
+                } else {
+                    throw new Error(element + " is not a function")
+                }
+            } catch (err) {
+                console.log(err);
+                return err.message;
+            }
+        }
+
+        function _search(name) {
+            for (var i in queue) {
+                if (queue[i].name == name)
+                    return i;
+            }
+            return false;
+        }
+
+        function deQueue(name) {
+            if (name)
+                var index = _search(name);
+            if (index != null) {
+                var func = queue.splice(index, 1);
+                return func[0]();
+            } else
+                queue.shift();
+        }
+
+        function _isFunction(elem) {
+            if (typeof elem == "function")
+                return true;
+            else
+                return false;
+        }
+
+        function _strict(callback) {
+
+            if (queue.length > 0) {
+                waitForAngular(function() {
+                    $q.resolve(queue.shift()(), function(args) {
+                        if (args != null) result.push(args);
+                        else result.push("");
+
+                        callback(_strict);
+                    }, function() {
+                        if (args != null) result.push(args);
+                        else result.push("");
+
+                        callback(_strict);
+                    });
+                });
+            }
+        }
+
+        function _execute(callback) {
+            $q.resolve(queue.shift()(), function(args) {
+                if (queue.length > 0) {
+                    callback(_execute);
+                }
+            }, function() {
+                if (queue.length > 0) {
+                    callback(_execute);
+                }
+            });
+        }
+
+        function initQueue(value) {
+            if (!value) {
+                var promise;
+                if (queue.length > 0)
+                    _strict(_strict);
+            } else {
+                if (queue.length > 0)
+                    _execute(_execute);
+            }
+        }
+
+        return {
+            enQueue: enQueue,
+            deQueue: deQueue,
+            initQueue: initQueue
+        };
+
+
+    })();
+
+    return Manager;
+}]);
